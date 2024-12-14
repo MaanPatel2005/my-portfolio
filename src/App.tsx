4281c13598c5727ef396ae2475c4f71e31766e40
MaanPatel2005 <maanvpatel@gmail.com>
2024-12-13 22:19:37 -0800

    Landing page of portfolio

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./App.css";
+import linkedIn from "/linkedInPhoto.png";
+import Github from "/githubLogo.png";
+import ProfilePhoto from "/newpfp.png";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="logo-container">
+        <a href="https://www.linkedin.com/in/maanpatel111/" target="_blank">
+          <img src={linkedIn} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://github.com/MaanPatel2005" target="_blank">
+          <img src={Github} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div className="App">
+        <div className="content">
+          <div className="photo-frame">
+            <img
+              src={ProfilePhoto}
+              alt="My Profile"
+              className="profile-photo"
+            />
+          </div>
+          <div className="text-section">
+            <h1 className="name">Hey, I'm Maan!</h1>
+            <div className="dotted-line"></div>{" "}
+            {/* Dotted line between photo and name */}
+            <p className="subtitle">Software Engineer | AI Enthusiast</p>
+            <p className="description">
+              3rd Year Honors Computer Science at University of California,
+              Irvine. Passionate about technology, coding, and solving complex
+              problems.
+            </p>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <p>&copy; 2024 Maan Patel. All rights reserved.</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
