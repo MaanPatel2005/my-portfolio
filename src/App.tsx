ed91ab41dbb77d597efe7a7d5f64b1e34c852c7a
MaanPatel2005 <maanvpatel@gmail.com>
2024-12-13 22:23:02 -0800

    testing commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ProfilePhoto from "/newpfp.png";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  // This is the landing page
   return (
     <>
       <div className="logo-container">
