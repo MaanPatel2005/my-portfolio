f2aa911b1efa43b7f9072fca45445425b637d896
MaanPatel2005 <maanvpatel@gmail.com>
2024-12-18 00:55:29 -0800

    Added in fully funtional About Me page

--- src/aboutMe.tsx
+++ src/aboutMe.tsx
@@ -2,7 +2,10 @@ import "./home.css";
 import "./aboutMe.css";
 import linkedIn from "/linkedInPhoto.png"; // Ensure this is in the public folder
 import Github from "/githubLogo.png"; // Ensure this is in the public folder
-import ProfilePhoto from "/newpfp.png"; // Ensure this is in the public folder
+import India from "/indiaGate.png";
+import Potato from "/potatoFritter.png";
+import Solo from "/solo.png";
+import PostedPic from "/posted.png";
 import { Provider } from "./components/ui/provider";
 import { Button } from "./components/ui/button";
 import {
@@ -73,7 +76,60 @@ function aboutMe() {
             </a>
           </div>
         </div>
-
+        <div className="body-section">
+          <div className="text-section-About">
+            <div className="about-me-container">
+              <h1 className="about-me-intro">A little bit about me: </h1>
+            </div>
+            <div className="summary">
+              {" "}
+              <p>
+                {" "}
+                My name is Maan Patel, and I am currently a second-year student
+                at the University of California, Irvine, pursuing an Honors
+                degree in Computer Science. I am passionate about leveraging
+                both business consulting and technology solutions to creatively
+                address complex problems in the industry.{" "}
+              </p>{" "}
+              <p></p>{" "}
+              <p>
+                {" "}
+                Beyond academics, I serve as a Resident Advisor within my
+                housing community, collaborating with department heads in
+                Information & Computer Sciences and Engineering to organize
+                events that enable residents to expand their networks and
+                explore research opportunities at UCI.{" "}
+              </p>{" "}
+              <p>
+                {" "}
+                In addition, I take the initiative to offer tutoring sessions
+                for ICS 46, UCI's data structures and algorithms course. Due to
+                a lack of budget for official tutoring services, I proactively
+                launched my own free tutoring program to support students in
+                mastering this critical subject. As a result, I helped students
+                improve their grades by an average of 10%, and my program has
+                grown to accommodate up to 15 students per session. Although it
+                is still in its early stages, I am working on scaling this
+                initiative to support other courses in the future.{" "}
+              </p>{" "}
+              <p>
+                {" "}
+                In my free time, I enjoy staying active through fitness,
+                spending quality time with family, and traveling.{" "}
+              </p>{" "}
+            </div>
+          </div>
+          <div className="pictures-About">
+            <div className="topPics">
+              <img src={India} className="IndiaGate" alt="India Gate" />
+              <img src={PostedPic} className="Posted" alt="Patvi Men" />
+            </div>
+            <div className="bottomPics">
+              <img src={Solo} className="SoloPic" alt="Beach" />
+              <img src={Potato} className="PotatoFrit" alt="Potato Fritter" />
+            </div>
+          </div>
+        </div>
         <footer>
           <p>&copy; 2024 Maan Patel. All rights reserved.</p>
         </footer>
